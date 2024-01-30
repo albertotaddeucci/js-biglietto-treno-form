92867ce639acb99d9a8d2f4b42b299835d18fdd6
alberto <alberto.taddeucci@gmail.com>
2024-01-30 14:48:27 +0100

    funzionamento button ( no input)

---
+++ js/script.js
@@ -0,0 +1,119 @@
+const buttonElement = document.querySelector("#button");
+
+// prova
+const titleElement = document.querySelector("h1");
+
+buttonElement.addEventListener("click",
+    function(){
+        // prova
+        titleElement.innerText = "ciao"
+
+        // algoritmo
+
+        const userAge = 15;
+        const distance = 100;
+        // let userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
+
+       
+        // controllo anno
+        // if(userYear<1900 || isNaN(userYear) || userYear>currentYear) {
+        //     alert("Attenzione: data inserita non valida");
+        //     userYear = "errore"   
+        // }
+
+        // // let distance = prompt("Quanti chilometri devi percorrere?");
+
+        // // controllo distanza
+
+        // if (isNaN(distance) || distance<1){
+        //     alert("Attenzione: distanza inserita non valida");
+        //     distance = "errore"
+        // }
+
+
+        // costo biglietto intero
+        const ticketPrice = 0.21 * distance;
+
+        // calcolo sconto    
+        let discountPrice;
+
+        if (userAge<18){
+            discountPrice = ticketPrice * 0.2;
+            
+        } else if (userAge>65){
+            discountPrice = ticketPrice * 0.4;
+            
+        } else {
+            discountPrice = 0;
+        }
+
+        // biglietto scontato
+        const finalPrice = ticketPrice - discountPrice;
+
+
+        // messaggio errore o visualizzazione a schermo prezzo
+        // if (userYear === "errore" || distance === "errore"){
+        //     document.getElementById("ticketprice").innerHTML = "Errore"
+        // } else {
+        //     document.getElementById("ticketprice").innerHTML = 
+        //     `<b>${finalPrice.toFixed(2)}€</b>`
+        // }
+
+        document.getElementById("ticketprice").innerHTML = 
+            `<b>${finalPrice.toFixed(2)}€</b>`
+      
+
+
+    }
+)
+
+// const date = new Date();
+// // let userYear = prompt("Inserisci anno di nascita (formato: YYYY)");
+
+// const userAge = date.getFullYear()
+
+// // controllo anno
+// if(userYear<1900 || isNaN(userYear) || userYear>currentYear) {
+//     alert("Attenzione: data inserita non valida");
+//     userYear = "errore"   
+// }
+
+// // let distance = prompt("Quanti chilometri devi percorrere?");
+
+// // controllo distanza
+
+// if (isNaN(distance) || distance<1){
+//     alert("Attenzione: distanza inserita non valida");
+//     distance = "errore"
+// }
+
+// // calcolo età
+// const userAge = currentYear - userYear;
+
+// // costo biglietto intero
+// const ticketPrice = 0.21 * distance;
+
+// // calcolo sconto    
+// let discountPrice;
+
+// if (userAge<18){
+//     discountPrice = ticketPrice * 0.2;
+      
+// } else if (userAge>65){
+//     discountPrice = ticketPrice * 0.4;
+      
+// } else {
+//     discountPrice = 0;
+// }
+
+// // biglietto scontato
+// const finalPrice = ticketPrice - discountPrice;
+
+
+// // messaggio errore o visualizzazione a schermo prezzo
+// if (userYear === "errore" || distance === "errore"){
+//     document.getElementById("ticketprice").innerHTML = "Errore"
+// } else {
+//     document.getElementById("ticketprice").innerHTML = 
+//     `<b>${finalPrice.toFixed(2)}€</b>`
+// }
